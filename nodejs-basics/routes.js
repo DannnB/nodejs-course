b1d2a011c84d35ee7c244f22f46fc1ee32b3c749
DannnB <dbroughton_09@hotmail.com>
2018-12-29 00:25:35 +0000

    finish debugging module

--- nodejs-basics/routes.js
+++ nodejs-basics/routes.js
@@ -7,7 +7,7 @@ const requestHandler = (req, res) => {
     if(url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>'); 
-        res.write('<head><title>My First Page</title></head>'); 
+        res.write('<head><title>App</title></head>'); 
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'); 
         res.write('</html>'); 
         return res.end();
