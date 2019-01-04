0d8666e825d6e3c0ef3c7507287f71e0cc94b6db
DannnB <dbroughton_09@hotmail.com>
2019-01-04 20:27:24 +0000

    add brief for nav example

---
+++ assignments/assignment-03-navigation/app.js
@@ -0,0 +1,5 @@
+// create npm project and use nodemon if needed
+
+// create express and serve 2 HTML files (own content) for the routes '/' and 'users'
+
+// add static file (css, js) and include it in at least one HTML file - dosn't have to have content in
